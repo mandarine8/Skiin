7220b4bc6a9ac7949604ff7d4e5eb5656794a4a3
Amandine Car <caramandine@gmail.com>
2019-11-27 13:39:15 -0500

    caroussel stuffs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_203318) do
+ActiveRecord::Schema.define(version: 2019_11_27_035351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_203318) do
     t.integer "number_of_slopes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pictures", array: true
   end
 
   create_table "skivents", force: :cascade do |t|
