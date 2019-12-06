91c32215bd5501d560e6c85dbe768a273025681c
Amandine Car <caramandine@gmail.com>
2019-12-06 01:03:50 -0500

    cards skivents cropped, style other user, homogeneisation dashboard, requests update, other info cards update, seeds and more

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,7 @@ class BookingsController < ApplicationController
       status: "pending"
     )
     @booking.save!
+    flash[:notice] = "The Skivent has been booked!"
     redirect_to user_path(current_user)
   end
 
@@ -16,8 +17,10 @@ class BookingsController < ApplicationController
   def update
     @booking = Booking.find(params[:id])
     if params[:booking][:status] == "accepted"
+      flash[:notice] = "The request has been accepted!"
       @booking.update!(status: "accepted")
     elsif params[:booking][:status] == "declined"
+      flash[:notice] = "The request has been declined!"
       @booking.update!(status: "declined")
     end
     redirect_to user_path(@booking.skivent.user)
